89990ffbea9c970ac94b848aa12cfe2a3e48c288
kchaurasia <kchaurasia@PF1C1A1B.ad.wcpss.net>
2022-01-24 22:33:21 -0500

    I am uploading my game

---
+++ src/Mario/Stage.java
@@ -0,0 +1,12 @@
+package Mario;
+
+public class Stage {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("welcome to the game");
+        System.out.println("Regards Kirtan");
+	}
+
+}
