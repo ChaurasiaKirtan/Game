06e7564a6243d2ed47b89911d750b077b3e6e7f8
kchaurasia <kchaurasia@PF1C1A1B.ad.wcpss.net>
2022-01-24 23:01:21 -0500

    Adding Comments in Programing

--- src/Mario/Stage.java
+++ src/Mario/Stage.java
@@ -4,7 +4,7 @@ public class Stage {
 
 	public static void main(String[] args) {
 		
-		
+		//Code Lab Training Sessions
 		System.out.println("welcome to the game");
 		System.out.println("Designed and Developed by Kirtan");
 		System.out.println("Regards Kirtan");
